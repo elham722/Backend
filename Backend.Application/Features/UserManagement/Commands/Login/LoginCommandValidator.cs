99bd3614a38ff950d6b1f52a3549ecd13b0ee150
elham722 <elham.heydari722@gmail.com>
2025-09-01 15:45:12 +0330

    correct basevalidator and login validator

--- Backend.Application/Features/UserManagement/Commands/Login/LoginCommandValidator.cs
+++ Backend.Application/Features/UserManagement/Commands/Login/LoginCommandValidator.cs
@@ -1,3 +1,4 @@
+using Backend.Application.Common.Validation;
 using FluentValidation;
 
 namespace Backend.Application.Features.UserManagement.Commands.Login;
@@ -5,22 +6,19 @@ namespace Backend.Application.Features.UserManagement.Commands.Login;
 /// <summary>
 /// Validator for LoginCommand
 /// </summary>
-public class LoginCommandValidator : AbstractValidator<LoginCommand>
+public class LoginCommandValidator : BaseValidator<LoginCommand>
 {
     public LoginCommandValidator()
     {
-        RuleFor(x => x.EmailOrUsername)
-            .NotEmpty().WithMessage("Email or username is required")
-            .MaximumLength(256).WithMessage("Email or username cannot exceed 256 characters");
+        // ????? ?? ???????
+        ValidateRequiredString(x => x.EmailOrUsername, 256);
 
-        RuleFor(x => x.Password)
-            .NotEmpty().WithMessage("Password is required")
-            .MaximumLength(128).WithMessage("Password cannot exceed 128 characters");
+        // ?????
+        ValidateRequiredString(x => x.Password, 128);
 
-        RuleFor(x => x.TwoFactorCode)
-            .MaximumLength(10).When(x => !string.IsNullOrEmpty(x.TwoFactorCode))
-            .WithMessage("Two-factor code cannot exceed 10 characters")
-            .Matches(@"^\d+$").When(x => !string.IsNullOrEmpty(x.TwoFactorCode))
-            .WithMessage("Two-factor code must contain only digits");
+        // ?? ?? ?????
+        ValidateOptionalString(x => x.TwoFactorCode, 10)
+            .Matches(@"^\d+$").WithMessage("Two-factor code must contain only digits");
     }
-} 
+
+}
