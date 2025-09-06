78e7f9b0a254fca57daccbf4e59e48b289fc6cf2
elham722 <elham.heydari722@gmail.com>
2025-09-06 15:53:53 +0330

    problem with load swagger

--- Backend.Api/Program.cs
+++ Backend.Api/Program.cs
@@ -155,7 +155,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "Backend API v1");
-        options.RoutePrefix = string.Empty; // Serve Swagger UI at root
+        options.RoutePrefix = "swagger"; // Serve Swagger UI at /swagger
     });
 }
 
